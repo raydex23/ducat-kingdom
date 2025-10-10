50c0abade7cd1314b8c7402e01935a46d166f5d4
raydex23 <krzysiek.raydex@gmail.com>
2025-10-10 21:49:05 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+// src/App.jsx
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { useWallet } from "@solana/wallet-adapter-react";
 
-import WalletConnectionProvider from "./WalletConnectionProvider";
 import Game from "./Game";
 import Ranking from "./Ranking";
 import Tavern from "./Tavern";
@@ -11,24 +11,18 @@ import Landing from "./Landing";
 
 function HomeRouter() {
   const { connected } = useWallet();
-
-  // proste sprawdzenie – jeśli wallet połączony, gra, inaczej landing
   return connected ? <Game /> : <Landing />;
 }
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <WalletConnectionProvider>
-        <Routes>
-          <Route path="/" element={<HomeRouter />} />
-          <Route path="/game" element={<Game />} />
-          <Route path="/ranking" element={<Ranking />} />
-          <Route path="/tavern" element={<Tavern />} />
-          <Route path="/pleasure" element={<PleasureHouse />} />
-          <Route path="*" element={<HomeRouter />} />
-        </Routes>
-      </WalletConnectionProvider>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomeRouter />} />
+      <Route path="/game" element={<Game />} />
+      <Route path="/ranking" element={<Ranking />} />
+      <Route path="/tavern" element={<Tavern />} />
+      <Route path="/pleasure" element={<PleasureHouse />} />
+      <Route path="*" element={<HomeRouter />} />
+    </Routes>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import WalletConnectionProvider from "./WalletConnectionProvider";
 import { BrowserRouter } from "react-router-dom";
+import WalletConnectionProvider from "./WalletConnectionProvider";
 import App from "./App.jsx";
 import "./index.css";
 
