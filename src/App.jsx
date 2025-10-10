f83483e0f55cd47c8f3b9889a93caaa6b5c510d9
raydex23 <krzysiek.raydex@gmail.com>
2025-10-10 20:01:28 +0200

    brothel

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Game from "./Game";
 import Ranking from "./Ranking";
 import Tavern from "./Tavern";
+import PleasureHouse from "./PleasureHouse";
 import WalletConnectionProvider from "./WalletConnectionProvider";
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Game />} />
         <Route path="/ranking" element={<Ranking />} />
         <Route path="/tavern" element={<Tavern />} />
+        <Route path="/pleasure" element={<PleasureHouse />} />
       </Routes>
     </WalletConnectionProvider>
   );
