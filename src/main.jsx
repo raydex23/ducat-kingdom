e9796b756473e368cab7770a460645636a3fffbf
raydex23 <krzysiek.raydex@gmail.com>
2025-10-10 21:44:58 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import WalletConnectionProvider from "./WalletConnectionProvider";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
@@ -7,7 +8,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <WalletConnectionProvider>
+        <App />
+      </WalletConnectionProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
